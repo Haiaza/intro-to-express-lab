2fa8962184633c662f0c5f8c2bbdc54da7d455ee
haiaza <83097184+Haiaza@users.noreply.github.com>
2024-06-28 21:25:46 +0000

    working on Task 2

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ app.get("/greetings/:username", (req, res) => {
   res.send(`I hope you have a great day ${req.params.username}`);
 });
 
-
+app.get('/roll/:number', (req, res) => {
+  req =  parseInt(req)
+  if (typeof req.params.number === 'number'){
+    console.log('Number detected')
+  }
+})
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
