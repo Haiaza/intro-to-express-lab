065037462f1962d40364cc61034e45700e3870be
haiaza <ajames1342@gmail.com>
2024-06-29 14:12:06 -0400

    Task3 looks good

--- server.js
+++ server.js
@@ -35,3 +35,22 @@ app.get('/roll/:number', (req, res) => {
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
+
+//task 3 
+
+app.get('/collectibles/:indexNum', (req, res) => {
+  let name = req.params.indexNum.name
+  let price = req.params.indexNum.price
+
+  const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+
+  collectibles.forEach((index) => {
+    res.send(`<h1>Looks like you wanted the ${index.name}? For only $${index.price} it's yours!</h1>`)
+  })
+})
