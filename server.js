5c297d9b89188998c8f2bb835189781740c06f0c
haiaza <ajames1342@gmail.com>
2024-06-29 13:24:09 -0400

    task 2ompleted

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/roll/:number', (req, res) => {
   if (typeof number === 'number'){
 
 
-    res.send(`<h1>${Math.floor(Math.random()) * number}</h1>`)
+    res.send(`<h1>${Math.floor(Math.random()* number) }</h1>`)  
     console.log(`${number} is our selected number`)
   }
 })
